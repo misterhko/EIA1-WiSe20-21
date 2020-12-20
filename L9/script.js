2bbe33db28c4b58803cce9ffe8761d41f64ea1d6
misterhko <72976605+misterhko@users.noreply.github.com>
2020-12-20 15:53:40 +0100

    Upload L9

---
+++ L9/script.js
@@ -0,0 +1,45 @@
+// Vorgabe Eingabefeld
+var inputField = document.getElementById("text");
+// Enter als Eingabe definieren
+inputField.addEventListener("keypress", function (event) {
+    if (event.key == "Enter") {
+        AufgabeErstellen();
+        inputField.value = "";
+    }
+});
+// Eingabefeld leeren nach Enter
+var removeAll = (document.querySelector(".fas fa-redo"));
+// Aufgaben Zähler
+var aufgaben = 0;
+function Gesamtaufgaben() {
+    document.getElementById("Gesamtaufgaben").innerText = aufgaben + " : 0 für die Faulheit!";
+}
+// Aufgabe hinzufügen
+function AufgabeErstellen() {
+    var myCheckbox = document.createElement("input");
+    myCheckbox.type = "checkbox";
+    myCheckbox.className = "checkbox";
+    var myLabel = document.createElement("label");
+    myLabel.innerHTML = inputField.value;
+    myLabel.className = "text";
+    var deleteBtn = document.createElement("i");
+    deleteBtn.className = "fas fa-trash-alt";
+    var wrapper = document.createElement("div");
+    document.getElementById("form").appendChild(wrapper);
+    wrapper.appendChild(myCheckbox);
+    wrapper.appendChild(myLabel);
+    wrapper.appendChild(deleteBtn);
+    // Tasks löschen
+    deleteBtn.addEventListener("click", function () {
+        deleteText(deleteBtn, wrapper);
+    });
+    aufgaben++;
+    Gesamtaufgaben();
+}
+// Funktion für Deletebutton
+function deleteText(deleteBtn, wrapper) {
+    wrapper.remove();
+    aufgaben--;
+    Gesamtaufgaben();
+}
+//# sourceMappingURL=script.js.map
---
+++ L9/script.ts
@@ -0,0 +1,54 @@
+// Vorgabe Eingabefeld
+    const inputField: HTMLInputElement = (document.getElementById("text") as HTMLInputElement);
+
+// Enter als Eingabe definieren
+    inputField.addEventListener("keypress", function (event: KeyboardEvent): void {
+        if (event.key == "Enter") {
+            AufgabeErstellen();
+            inputField.value = "";
+        }
+    });
+
+// Eingabefeld leeren nach Enter
+    const removeAll: HTMLElement = (document.querySelector(".fas fa-redo"));
+    
+// Aufgaben Zähler
+    let aufgaben: number = 0;
+    function Gesamtaufgaben(): void {
+        document.getElementById("Gesamtaufgaben").innerText = aufgaben + " : 0 für die Faulheit!";
+    }
+
+// Aufgabe hinzufügen
+    function AufgabeErstellen(): void {
+        let myCheckbox: HTMLInputElement = document.createElement("input");
+        myCheckbox.type = "checkbox";
+        myCheckbox.className = "checkbox";
+
+        let myLabel: HTMLLabelElement = document.createElement("label");
+        myLabel.innerHTML = inputField.value;
+        myLabel.className = "text";
+
+        let deleteBtn: HTMLElement = document.createElement ("i");
+        deleteBtn.className = "fas fa-trash-alt";
+
+        let wrapper: HTMLDivElement = document.createElement ("div");
+
+        document.getElementById("form").appendChild(wrapper);
+        wrapper.appendChild(myCheckbox);
+        wrapper.appendChild(myLabel);
+        wrapper.appendChild(deleteBtn);
+
+// Tasks löschen
+        deleteBtn.addEventListener("click", function(): void {
+            deleteText(deleteBtn, wrapper);
+        });     
+        aufgaben++;
+        Gesamtaufgaben();
+        }
+    
+// Funktion für Deletebutton
+    function deleteText(deleteBtn: HTMLElement, wrapper: HTMLDivElement): void {
+        wrapper.remove();
+        aufgaben--;
+        Gesamtaufgaben();
+    }   
